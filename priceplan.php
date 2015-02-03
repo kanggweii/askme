54e78691cc5b91cac21b007b135bb9944f39d9d4
kanggweii <tankangwei91@gmail.com>
2015-02-03 08:48:39 +0800

    Create priceplan.php

---
+++ priceplan.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>AskMe</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/landing-page.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="http://ukkk45f01281.kanggweii.koding.io/">ASK</a>
+                    </li>
+                    <li>
+                        <a href="answers.php">Answers</a>
+                    </li>
+                    <li>
+                        <a href="profile.php">Profile</a>
+                    </li>
+                    <li>
+                        <a href="priceplan.php">Services</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+<center>
+    <a  name="services"></a>
+        <div class="content-section-a">
+    <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Join US!
+                            <small>Our Pricing Options</small>
+                        </h1>
+                    </div>
+            
+                </div>
+        
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="panel panel-default text-center">
+                            <div class="panel-heading">
+                                <strong>AskMe</strong>
+                            </div>
+                        <div class="panel-body">
+                            <h3 class="panel-title price">Free!
+                            </h3>
+                        </div>
+                        <ul class="list-group">
+                            <li class="list-group-item">5 Questions per Day</li>
+                            <li class="list-group-item">Normal Points Growth</li>
+                            <li class="list-group-item">Up to 10 Answers</li>
+    
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="panel panel-default text-center">
+                        <div class="panel-heading">
+                            <strong>AskPro</strong>
+                        </div>
+                        <div class="panel-body">
+                            <h3 class="panel-title price">$30
+                                <span class="price-cents">00</span>
+                                <span class="price-month">mo.</span>
+                            </h3>
+                        </div>
+                        <ul class="list-group">
+                            <li class="list-group-item">50 Questions per Day</li>
+                            <li class="list-group-item">Enhanced Points Growth</li>
+                            <li class="list-group-item">Up to 50 Answers</li>
+                            <li class="list-group-item">First 20 Questions gets Priority Queue</li>
+                            <li class="list-group-item"><a class="btn btn-primary">Sign Up Now!</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="panel panel-default text-center">
+                        <div class="panel-heading">
+                            <strong>NeverStopAsking! <span class="label label-success">Best Value!</span></strong>
+                        </div>
+                        <div class="panel-body">
+                            <h3 class="panel-title price">$39
+                                <span class="price-cents">99</span>
+                                <span class="price-month">mo.</span>
+                            </h3>
+                        </div>
+                        <ul class="list-group">
+                            <li class="list-group-item">Unlimited Questions per Day</li>
+                            <li class="list-group-item">Ultimate Points Growth</li>
+                            <li class="list-group-item">Unlimited Answers</li>
+                            <li class="list-group-item">All Questions gets Highest Priority Queue</li>
+                            <li class="list-group-item"><a class="btn btn-primary">Sign Up Now!</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <!-- /.container -->
+    
+    
+        </div>
+        <!-- /.content-section-b -->
+</center>   
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
